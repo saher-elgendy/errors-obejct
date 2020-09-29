74f4cfd5de9b71c2dd5b01adb6b9b8049fefb47c
unknown <saherelgendy1990@gmail.com>
2020-09-29 03:45:36 -0700

    update js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Listen for submit
-document.getElementById('requirementsForm').addEventListener('submit', function(e){
+document.getElementById('requirementsForm').addEventListener('submit', function (e) {
   e.preventDefault();
 
   // Hide results
@@ -7,20 +7,20 @@ document.getElementById('requirementsForm').addEventListener('submit', function(
 
   // Show loadingImg
   showLoadingImg();
-  
+
   // Run calculateFinalResults function after two seconds
   setTimeout(calculateFinalResults, 2000);
 });
 
 // Assign UI variables
-  // Requirements variables
-  const inputLoanAmount = document.querySelector('.inputLoanAmount');
-  const inputInterest = document.querySelector('.inputInterest');
-  const inputYearsToRepay = document.querySelector('.inputYearsToRepay');
-  // Results variables
-  const inputMonthlyPayment = document.querySelector('.inputMonthlyPayment');
-  const inputTotalPayment = document.querySelector('.inputTotalPayment');
-  const inputTotalInterest = document.querySelector('.inputTotalInterest');
+// Requirements variables
+const inputLoanAmount = document.querySelector('.inputLoanAmount');
+const inputInterest = document.querySelector('.inputInterest');
+const inputYearsToRepay = document.querySelector('.inputYearsToRepay');
+// Results variables
+const inputMonthlyPayment = document.querySelector('.inputMonthlyPayment');
+const inputTotalPayment = document.querySelector('.inputTotalPayment');
+const inputTotalInterest = document.querySelector('.inputTotalInterest');
 
 // Create calculateFinalResults function
 
@@ -34,34 +34,38 @@ const inputs = [...document.getElementsByClassName('form-control')];
 
 inputs.forEach(input => {
   input.addEventListener('change', (e) => {
-    inputValues = {
-      ...inputValues,
+    // inputValues = {
+    //   ...inputValues,
+    //   [e.target.name]: e.target.value
+    // }
+    inputValues = Object.assign(inputValues, {
       [e.target.name]: e.target.value
-    }
+    })
   });
 });
 
 const isEmpty = (input) => {
-  if(input.trim() === '') return true;
+  if (input.trim() === '') return true;
   return false;
 }
 
 
 const validate = (inputValues) => {
+  console.log('ho', inputValues)
   let errors = {};
-  if(isEmpty(inputValues.loan)) errors.loan = 'Loan Must not be empty';
-  if(isEmpty(inputValues.interest)) errors.interest = 'Interest must not be empty';
-  if(isEmpty(inputValues.total)) errors.total = 'Total Pay must not be empty';
+  if (isEmpty(inputValues.loan)) errors.loan = 'Loan Must not be empty';
+  if (isEmpty(inputValues.interest)) errors.interest = 'Interest must not be empty';
+  if (isEmpty(inputValues.total)) errors.total = 'Total Pay must not be empty';
   return errors
 }
 // console.log(errors)
-function calculateFinalResults(loadingImg,results) {
+function calculateFinalResults(loadingImg, results) {
   const errors = validate(inputValues);
 
   if (Object.keys(errors).length) {
-   if(errors.loan) alert('loan must not be empty')
-   if(errors.total) alert('total must not be empty')
-   if(errors.interest) alert('interest must not be empty')
+    if (errors.loan) alert('loan must not be empty')
+    if (errors.total) alert('total must not be empty')
+    if (errors.interest) alert('interest must not be empty')
     // Hide results and loadingImg
     hideLoadingImg();
     hideResults();
@@ -89,7 +93,7 @@ function calculateFinalResults(loadingImg,results) {
     const calculateTotalPayment = parseFloat(Number(inputLoanAmount.value) + calculateTotalInterest);
     inputTotalPayment.value = calculateTotalPayment.toFixed(2);;
     // calculate Monthly Payment
-    const calculateMonthlyPayment = parseFloat(calculateTotalPayment / (Number(inputYearsToRepay.value)*12));
+    const calculateMonthlyPayment = parseFloat(calculateTotalPayment / (Number(inputYearsToRepay.value) * 12));
     inputMonthlyPayment.value = calculateMonthlyPayment.toFixed(2);
     // Hide loadingImg and show results
     hideLoadingImg();
@@ -112,18 +116,18 @@ function removeErrorDiv() {
 }
 
 // Show loadingImg function
-function showLoadingImg(){
+function showLoadingImg() {
   document.querySelector('.loadingImg').setAttribute('style', 'display: block !important');
 }
 // Hide loadingImg function
-function hideLoadingImg(){
+function hideLoadingImg() {
   document.querySelector('.loadingImg').setAttribute('style', 'display: none !important');
 }
 // Show results function
-function showResults(){
+function showResults() {
   document.querySelector('.results').setAttribute('style', 'display: block !important');
 }
 // Hide results function
-function hideResults(){
+function hideResults() {
   document.querySelector('.results').setAttribute('style', 'display: none !important');
 }
